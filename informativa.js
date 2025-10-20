b4d011f65fedf178d9d1326b4184ca1ddb5229a8
tartinez28 <miguel.martinezt@uniagustiniana.edu.co>
2025-10-19 20:36:40 -0500

    Update app information in informativa.js

--- informativa.js
+++ informativa.js
@@ -1,8 +1,12 @@
 function informativaPage() {
   const contenedor = document.getElementById("content");
   contenedor.innerHTML = `
-    <h2>Información de la App</h2>
-    <p>Esta aplicación consume datos de <strong>FakerAPI.it</strong> para mostrar información simulada de personas.</p>
-    <p>Desarrollada como proyecto educativo.</p>
+    <h2>Acerca de la App</h2>
+    <div class="item">
+      <p><b>Nombre:</b> FakerAPI App</p>
+      <p><b>Desarrolladora:</b> Sara Giraldo</p>
+      <p><b>Descripción:</b> Aplicación web conectada a una API que muestra personas ficticias, permite buscar, ver detalles y guardar favoritos.</p>
+      <p><b>Versión:</b> 1.0</p>
+    </div>
   `;
 }
