4cdf8fc732e89fa6c817d159c3dec65c6a05062d
tartinez28 <miguel.martinezt@uniagustiniana.edu.co>
2025-10-19 20:35:04 -0500

    Update API endpoint to fetch users in Spanish

--- conexion.js
+++ conexion.js
@@ -1,10 +1,10 @@
 async function obtenerDatos() {
   try {
-    const response = await fetch("https://fakerapi.it/api/v1/persons?_quantity=10");
+    const response = await fetch("https://fakerapi.it/api/v1/users?_locale=es_ES&_quantity=10");
     const data = await response.json();
     return data.data;
   } catch (error) {
     console.error("Error al obtener datos:", error);
     return [];
-  } 
+  }
 }
