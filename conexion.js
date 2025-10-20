360505f5de7a01d396bee236290c53af36a2f959
tartinez28 <miguel.martinezt@uniagustiniana.edu.co>
2025-10-19 20:40:22 -0500

    Simplify obtenerDatos function by removing error handling

--- conexion.js
+++ conexion.js
@@ -1,10 +1,5 @@
 async function obtenerDatos() {
-  try {
-    const response = await fetch("https://fakerapi.it/api/v1/users?_locale=es_ES&_quantity=10");
-    const data = await response.json();
-    return data.data;
-  } catch (error) {
-    console.error("Error al obtener datos:", error);
-    return [];
-  }
+  const response = await fetch("https://fakerapi.it/api/v1/users?_quantity=10");
+  const data = await response.json();
+  return data.data;
 }
